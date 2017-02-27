41b58ff82ab48638cab71348f7da8fffd6761a76
Pierre-Etienne Mélet <pierre-etienne@melet.net>
2017-02-27 10:28:11 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react';
+import Masonry from 'react-masonry-component';
+import logo from './logo.svg';
+import './App.css';
+import Moment from 'moment';
+
+Moment.lang('fr');
+
+var rooturl = (window.location.hostname === "localhost") ? "http://titouan.melet-tary.fr" : "";
+var columnWidth = 90, gutter = 1;
+
+class Photo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      paysage: this.props.paysage,
+      portrait: this.props.portrait,
+      square: this.props.square,
+    }
+  }
+
+  updateOrientation(){
+    var paysage = false, portrait = false, square = false;
+    if(this.img.naturalWidth === this.img.naturalHeight) {
+      square = true;
+    } else if(this.img.naturalWidth > this.img.naturalHeight) {
+      paysage = true;
+    } else {
+      portrait = true;
+    }
+    console.log(this.img.naturalWidth / this.img.naturalHeight);
+    this.setState({ paysage, portrait, square })
+  }
+
+  zoomOn(name){
+    this.props.zoomOn(rooturl + "/web-sm/" + name);
+    return false;
+  }
+
+  // style={{ width: width-20, padding, margin: 8, borderRadius: 4, border: "2px solid white", boxShadow: "#888 0px 0px 10px 2px" }} 
+
+  render() {
+    var sizer = (this.state.portrait ? "r":(this.state.square ? "s":"p")),
+        padding = 0, margin = 0;
+    return (
+      <div className={"grid-item grid-item-"+sizer}>
+        <div>
+          <img 
+              onClick={this.zoomOn.bind(this,this.props.name)}
+              alt={this.props.name} 
+              src={rooturl + "/web-sm/"+this.props.name}
+              ref={(x) => { this.img = x }}
+              onLoad={this.updateOrientation.bind(this)}/>
+          <label>{Moment(this.props.name.split(".")[0], "YYYY-MM-DD-HHmm").subtract(55,"minutes").format("LLLL")}</label>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Section extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <Masonry options={{
+            // set itemSelector so .grid-sizer is not used in layout
+            itemSelector: '.grid-item',
+            // use element for option
+            columnWidth: '.grid-sizer',
+            percentPosition: true,
+          }}>
+          <div className="grid-sizer"/>
+          {
+            this.props.images.map((x,i) => <Photo key={i} name={x} zoomOn={this.props.zoomOn} />) 
+          }
+        </Masonry>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { }
+  }
+  
+  componentWillMount() {
+    fetch(rooturl + "/config.json")
+    .then((response) => response.json().then(config => this.setState({config}) ));
+  }
+
+  zoomOn(path){
+    this.state.selected = path;
+    this.setState(this.state);
+  }
+
+  render() {
+    return (
+      <article>
+        {
+          this.state.config
+           ? this.state.config.galleries.map((x,i) => <Section key={i} {...x} zoomOn={this.zoomOn.bind(this)}/>) 
+           : null
+        }
+        {
+          this.state.selected
+          ? <div className="modal">
+              <img src={this.state.selected} onClick={this.zoomOn.bind(this,null)}/>
+            </div>
+          : null
+        }
+      </article>
+    );
+  }
+}
+
+export default App;
